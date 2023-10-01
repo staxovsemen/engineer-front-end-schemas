2bfd9394c2b67feaabb0d0999021727644ea7c54
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-10-01 16:07:22 -0400

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,51 +1,52 @@
-}
-    public static void main(String[] args) {
-public class RandomDataGenerator {
-        }
-
-            data[i] = (int) (Math.random() * 100) + 1;
-
     }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
+
         for (int item : data) {
         }
         int[] data = new int[10];
-def generate_random_data():
-    for item in data:
-    return data
-def main():
-    data = generate_random_data()
-if __name__ == "__main__":
+}
+            System.out.println("Random Number: " + item);
+
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        }
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
 import random
-    main()
+    return data
+if __name__ == "__main__":
+
+    data = generate_random_data()
     data = [random.randint(1, 100) for _ in range(10)]
+    main()
+    for item in data:
 
-
+def generate_random_data():
         print(f"Random Number: {item}")
+def main():
+    return data
+
+
+
+def generate_random_data():
+    data = generate_random_data()
+def main():
+
+    main()
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    for item in data:
+    return data
 if __name__ == "__main__":
 
 
-
-    return data
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-    data = generate_random_data()
-
     main()
-def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def generate_random_data():
         print(f"Random Number: {item}")
-import random        }
-
-            data[i] = (int) (Math.random() * 100) + 1;
-public class RandomDataGenerator {
-        int[] data = new int[10];
-
-            System.out.println("Random Number: " + item);
-        for (int item : data) {
-    }
-        for (int i = 0; i < 10; i++) {
-}
-    public static void main(String[] args) {
-        }
+import random
+    for item in data:
+def main():
