216f3daa254e06168ffa70eda9aee884344d1d04
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-09-30 21:38:16 -0400

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,14 @@
-    return data
-
-
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-
-        print(f"Random Number: {item}")
-import random
-def generate_random_data():
-    data = generate_random_data()
     main()
 if __name__ == "__main__":
-def main():
+    data = generate_random_data()
+def generate_random_data():
+
+
     data = [random.randint(1, 100) for _ in range(10)]
-
-    return data
         print(f"Random Number: {item}")
-    main()
-
-if __name__ == "__main__":
-def main():
 
 import random
-    data = generate_random_data()
 
-def generate_random_data():
+def main():
     for item in data:
+    return data
