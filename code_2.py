8b9ce140c1bf75c049f813f5c46033ee9baf40a1
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-10-04 18:07:28 -0400

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,27 +1,56 @@
-import random
+    });
+
+
+
+function generateRandomData() {
+    return data;
+function main() {
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+}
+main();
+    const data = [];
+}
+    data.forEach(item => {
 def generate_random_data():
-
-    for item in data:
-def main():
-        print(f"Random Number: {item}")
-
     return data
-if __name__ == "__main__":
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-
-
-    main()    return data
-    data = generate_random_data()
-import random
-        print(f"Random Number: {item}")
 def main():
-
-if __name__ == "__main__":
-
-    for item in data:
-
-def generate_random_data():
-
     data = [random.randint(1, 100) for _ in range(10)]
     main()
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+
+    for item in data:
+
+import random    main()
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+def generate_random_data():
+def main():
+if __name__ == "__main__":
+
+    for item in data:
+
+
+import randomimport random
+    main()
+    data = generate_random_data()
+
+def main():
+    return data
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+if __name__ == "__main__":
