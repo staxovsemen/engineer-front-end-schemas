0f42bbce200f8c0cb337c48ad6f42febd847d1e4
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-10-10 21:59:17 -0400

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,66 +1,30 @@
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-    public static void main(String[] args) {
-        }
-        for (int i = 0; i < 10; i++) {
-
-        int[] data = new int[10];
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
-}
-        }
-
-    }        print(f"Random Number: {item}")
-    return data
-if __name__ == "__main__":
     data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    return data
+
+    for item in data:
+        print(f"Random Number: {item}")
+    main()
+def main():
+
 import random
 
-def generate_random_data():
-    for item in data:
+if __name__ == "__main__":function generateRandomData() {
 
 
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def main():}
-        for (int item : data) {
-        }
-    }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
-        }
-
-
-    public static void main(String[] args) {
-public class RandomDataGenerator {
-        int[] data = new int[10];
-            data[i] = (int) (Math.random() * 100) + 1;}
-    }
     const data = [];
-    for (let i = 0; i < 10; i++) {
-        console.log("Random Number: " + item);
-
-    const data = generateRandomData();
-    data.forEach(item => {
-main();
 function main() {
 
-
-function generateRandomData() {
-    return data;
-        data.push(Math.floor(Math.random() * 100) + 1);
 }
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+    }
     });
-import random
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-    for item in data:
-    data = generate_random_data()
-    main()
-def generate_random_data():
-
-if __name__ == "__main__":
-
-def main():
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+main();
+}
+    const data = generateRandomData();
