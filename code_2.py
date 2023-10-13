ac17761dcbc1293cbbf5fc4e9c1746c349f3cae1
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-10-13 05:24:55 -0400

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,59 +1,43 @@
-
-if __name__ == "__main__":
-def main():
-    data = generate_random_data()
-import random
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-    for item in data:
-    return data
-
-    main()
-        print(f"Random Number: {item}")
-
+    const data = generateRandomData();
         data.push(Math.floor(Math.random() * 100) + 1);
-    data.forEach(item => {
+main();
     }
         console.log("Random Number: " + item);
+}
+
 
     for (let i = 0; i < 10; i++) {
-}
-    const data = generateRandomData();
-    const data = [];
     });
-function generateRandomData() {
     return data;
-}
-main();
+function generateRandomData() {
+    const data = [];
 
-function main() {    main()
+    data.forEach(item => {
+function main() {
+}    for item in data:
 
 
-
-        print(f"Random Number: {item}")
     return data
-    for item in data:
-def generate_random_data():
-import random
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
 
     data = generate_random_data()
-if __name__ == "__main__":    }
-    const data = generateRandomData();
-main();
-        data.push(Math.floor(Math.random() * 100) + 1);
-    data.forEach(item => {
+def main():
 
-    for (let i = 0; i < 10; i++) {
-    return data;
-}
-}
-        console.log("Random Number: " + item);
-    const data = [];
-    });
+import random
+        print(f"Random Number: {item}")
+    main()
+if __name__ == "__main__":
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
 
-function generateRandomData() {
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+def main():
+    for item in data:
 
-function main() {
+    data = generate_random_data()
+    main()
+
+    return data
