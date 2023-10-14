8ea9ea560362aa83e246a236640f5487ecda89a2
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-10-13 23:54:01 -0400

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,39 +1,27 @@
-    public static void main(String[] args) {
-
-        for (int item : data) {
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-public class RandomDataGenerator {
-
-        for (int i = 0; i < 10; i++) {
-}
-        }
-    }
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];    main()
-
-if __name__ == "__main__":
-import random
-
-def generate_random_data():
-
     data = generate_random_data()
-    for item in data:
-        print(f"Random Number: {item}")
-    return data
 def main():
-    data = [random.randint(1, 100) for _ in range(10)]
         print(f"Random Number: {item}")
-def generate_random_data():
-    data = generate_random_data()
-if __name__ == "__main__":
+
     data = [random.randint(1, 100) for _ in range(10)]
 
-    for item in data:
-
-import random
-
-def main():
-
-    return data
+def generate_random_data():
     main()
+
+    for item in data:
+if __name__ == "__main__":
+import random
+
+    return data
+import random
+
+    data = generate_random_data()
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+def generate_random_data():
+    for item in data:
+
+    main()
+    return data
