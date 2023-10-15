34419bb12dd3386f2849f415a995fba06529822e
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-10-15 12:52:55 -0400

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,27 +1,42 @@
-    data = generate_random_data()
-def main():
-        print(f"Random Number: {item}")
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def generate_random_data():
-    main()
-
-    for item in data:
-if __name__ == "__main__":
-import random
-
-    return data
-import random
-
-    data = generate_random_data()
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-
-def generate_random_data():
-    for item in data:
 
     main()
+
+def generate_random_data():
+def main():
+import random
+if __name__ == "__main__":
     return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+    for item in data:    public static void main(String[] args) {
+        for (int item : data) {
+
+        for (int i = 0; i < 10; i++) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+            System.out.println("Random Number: " + item);
+    }
+        int[] data = new int[10];
+        }
+
+public class RandomDataGenerator {
+}
+
+main();
+    });
+function generateRandomData() {
+    return data;
+    const data = [];
+    data.forEach(item => {
+    const data = generateRandomData();
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+function main() {
