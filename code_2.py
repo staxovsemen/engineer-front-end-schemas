1473da461575435e86eab10cda95348ee9ae626b
staxovsemen <142354526+staxovsemen@users.noreply.github.com>
2023-10-16 07:21:42 -0400

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,29 +1,27 @@
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
 
-function generateRandomData() {
-}
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-    }
 
-    });
-        console.log("Random Number: " + item);
-main();
-}
-    return data;
-    data.forEach(item => {
-
-    const data = generateRandomData();    main()
-import random
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
 def main():
-    data = generate_random_data()
-def generate_random_data():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
+def generate_random_data():
+    data = generate_random_data()
 
+    main()
 
+    return data
     for item in data:
         print(f"Random Number: {item}")
+
+    main()
+if __name__ == "__main__":
+def main():
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def generate_random_data():
+
+    for item in data:
+    return data
+import random
